3474af41ca6c87b4ea527a501ee2d0c94f63762e
gradientcrux <phillipwaldo10@gmail.com>
2021-07-28 14:02:50 -0500

    change file name

--- mint_to_postgres.py
+++ mint_to_postgres.py
@@ -19,7 +19,7 @@ import cred
 
 # Get the IP of the system this script is coming from
 host_name = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
+host_ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
 
 mintUser = cred.user
 mintUserPassword = cred.mintPassword
@@ -394,7 +394,7 @@ def write_to_postgres(clean_df, table_name):
     try:
 
         clean_df.to_sql(table_name, postgreSQLConnection, if_exists='append')
-
+        print(f"You succeeded at {table_name}" )
     except ValueError as vx:
 
         print(vx)
