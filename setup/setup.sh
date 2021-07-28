3a404db26ef9908545635c9f67e7ea9ad919068c
gradientcrux <phillipwaldo10@gmail.com>
2021-07-28 13:00:07 -0500

    start of setup shell file

---
+++ setup/setup.sh
@@ -0,0 +1,24 @@
+# Get net tools so you can run ifconfig and get ip (if needed)
+sudo apt install net-tools 
+
+
+# Enable linux vm for remote desktop connection
+sudo apt install xrdp
+
+
+# Get chrome download
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Install google chrome
+sudo apt install ./googe-chrome-stable_current_amd64.deb
+
+# Install git
+sudo apt install git -y
+
+git config --user
+
+
+# Install Anaconda prerequisites
+sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+
+# Download 
